c2cc4902517ebbf0d6207423b51b18d0ad2881a8
Petermuturi <pmuturi65@gmail.com>
2017-06-20 19:54:11 +0300

    added dependencies and linked android/ios to one app

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Content } from 'native-base';
+import { View, Text } from 'react-native';
+
+export default ()=> {
+	return(
+		<Container>
+			<Content>
+				<View>
+					<Text>Hello World</Text>
+				</View>
+			</Content>
+		</Container>
+		)
+}
